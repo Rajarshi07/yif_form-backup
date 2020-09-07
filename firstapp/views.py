5f168bea3fef53d73c981cb15da9020435b5f34a
kcdeepak16 <60536433+kcdeepak16@users.noreply.github.com>
2020-09-07 14:17:38 +0530

    email1

--- firstapp/views.py
+++ firstapp/views.py
@@ -118,13 +118,13 @@ def paytm_gateway(request):
             text = "Hey, {name}\nYou have been registered for {event_name}.\nYour participant ID is: {participant_id}.\n"
             text = text.format(name=paid_registration.name, event_name=paid_registration.event,
                                participant_id=paid_registration.participant_id)
-            yag = yagmail.SMTP(user="prodigygamer143@gmail.com", password='8017586761')
+            """yag = yagmail.SMTP(user="prodigygamer143@gmail.com", password='8017586761')
             yag.send(
                 to=paid_registration.email,
                 subject=paid_registration.event + "Registration",
                 contents=text
-            )
-            """port = 587  # For starttls
+            )"""
+            port = 587  # For starttls
             smtp_server = "smtp.gmail.com"
             sender_email = event_registered.email
             receiver_email = paid_registration.email
@@ -147,7 +147,7 @@ def paytm_gateway(request):
                 server.starttls(context=context)
                 server.ehlo()  # Can be omitted
                 server.login(sender_email, password)
-                server.sendmail(sender_email, receiver_email, message.as_string())"""
+                server.sendmail(sender_email, receiver_email, message.as_string())
             state = events.objects.get(name = paid_registration.event).select_state
             return render(request, 'paytm_status.html', {'result' : True, 'details': paid_registration, 'state':state})
         else:
