e344a5c298586d46bd029d31b0ab2246d828a62d
kcdeepak16 <60536433+kcdeepak16@users.noreply.github.com>
2020-09-17 02:45:34 +0530

    coupons

--- firstapp/admin.py
+++ firstapp/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import events, registration, date_revenue, society_leads, state_connection
+from .models import events, registration, date_revenue, society_leads, state_connection, coupons
 
 # Register your models here.
 
@@ -7,6 +7,7 @@ admin.site.register(events)
 admin.site.register(date_revenue)
 admin.site.register(society_leads)
 admin.site.register(state_connection)
+admin.site.register(coupons)
 
 class RegistrationAdmin(admin.ModelAdmin):
     class Meta:
