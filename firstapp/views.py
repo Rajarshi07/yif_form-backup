e457e03d9ecddfbadd7d59fc8f4b006c38777e13
kcdeepak16 <60536433+kcdeepak16@users.noreply.github.com>
2020-09-24 11:45:32 +0530

    updates2

--- firstapp/views.py
+++ firstapp/views.py
@@ -172,6 +172,7 @@ def paytm_gateway(request):
             state = events.objects.get(name = paid_registration.event).select_state
             return render(request, 'paytm_status.html', {'result' : True, 'details': paid_registration, 'state':state})
         else:
+            paid_registration.link = response_dict['RESPCODE']
             state = events.objects.get(name=paid_registration.event).select_state
             return render(request, 'paytm_status.html', {'result' : False,  'state':state})
     
